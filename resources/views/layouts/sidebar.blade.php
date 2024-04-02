6c269d2fb305b7e2ff757adc5f90e0acbb812b42
Tony Ng <nguyencuonghut55@gmail.com>
2024-04-02 16:25:42 +0700

    Fix: improve header and sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,12 +4,12 @@
     @auth('admin')
     <a href="{{route('admin.home')}}" class="brand-link">
       <img src="{{ asset('images/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text">Honghafeed</span>
+      <span class="brand-text">{{Auth::user()->name}}</span>
     </a>
     @else
     <a href="{{route('user.home')}}" class="brand-link">
       <img src="{{ asset('images/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text">Honghafeed</span>
+      <span class="brand-text">{{Auth::user()->name}}</span>
     </a>
     @endauth
 
@@ -20,12 +20,9 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+          @auth('admin')
           <li class="nav-item">
-            @auth('admin')
             <a href="{{route('admin.home')}}" class="nav-link {{ Request::is('admin') ? 'active' : '' }}">
-            @else
-            <a href="{{route('user.home')}}" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
-            @endauth
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -34,14 +31,50 @@
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link {{ Request::is('tender*') ? 'active' : '' }}">
+            <a href="#" class="nav-link {{ Request::is('admin/profile*') ? 'active' : '' }}">
               <i class="nav-icon fas fa-id-card"></i>
               <p>
-                Hồ sơ
+                Hồ sơ của tôi
               </p>
             </a>
           </li>
 
+          <li class="nav-item">
+            <a href="{{route('admin.logout')}}" class="nav-link">
+              <i class="nav-icon fas fa-power-off"></i>
+              <p>
+                Đăng xuất
+              </p>
+            </a>
+          </li>
+          @else
+          <li class="nav-item">
+            <a href="{{route('user.home')}}" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link {{ Request::is('profile*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-id-card"></i>
+              <p>
+                Hồ sơ của tôi
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="{{route('user.logout')}}" class="nav-link">
+              <i class="nav-icon fas fa-power-off"></i>
+              <p>
+                Đăng xuất
+              </p>
+            </a>
+          </li>
+          @endauth
 
           @auth('admin')
           <li class="nav-header">HỆ THỐNG</li>
@@ -50,6 +83,14 @@
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Người dùng
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link {{ Request::is('admin/admins*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-user-lock"></i>
+              <p>
+                Người quản trị
               </p>
             </a>
           </li>
