f4336a7a0c9265135352574e8ddb1e963f2bfbae
Tony Ng <nguyencuonghut55@gmail.com>
2024-06-13 13:18:50 +0700

    Add some documents to Document seeder

--- database/seeders/DocumentsTableSeeder.php
+++ database/seeders/DocumentsTableSeeder.php
@@ -20,55 +20,118 @@ class DocumentsTableSeeder extends Seeder
             0 =>
                 array (
                     'id' => 1,
-                    'name' => 'Hợp đồng chính thức',
+                    'name' => 'Sơ yếu lý lịch',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             1 =>
                 array (
                     'id' => 2,
-                    'name' => 'Hợp đồng thử việc',
+                    'name' => 'Căn cước công dân',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             2 =>
                 array (
                     'id' => 3,
-                    'name' => 'Hợp đồng học việc',
+                    'name' => 'Giấy khám sức khỏe',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             3 =>
                 array (
                     'id' => 4,
-                    'name' => 'Thỏa thuận bảo mật thông tin',
+                    'name' => 'Giấy khai sinh',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             4 =>
                 array (
                     'id' => 5,
-                    'name' => 'Cam kết hội nhập',
+                    'name' => 'Số hộ khẩu/xác nhận cư trú',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             5 =>
                 array (
                     'id' => 6,
-                    'name' => 'Thỏa thuận thu hồi tạm ứng',
+                    'name' => 'Đơn xin việc',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             6 =>
                 array (
                     'id' => 7,
-                    'name' => 'Đăng ký người phụ thuộc (giấy ủy quyền + Mẫu số:20-ĐK-TCT',
+                    'name' => 'Bằng cấp',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ),
             7 =>
                 array (
                     'id' => 8,
+                    'name' => 'Giấy xác nhận nhân sự',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            8 =>
+                array (
+                    'id' => 9,
+                    'name' => 'Tờ khai',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            9 =>
+                array (
+                    'id' => 10,
+                    'name' => 'Giấy tờ khác',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            10 =>
+                array (
+                    'id' => 11,
+                    'name' => 'Cam kết hội nhập',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            11 =>
+                array (
+                    'id' => 12,
+                    'name' => 'HĐ thử việc',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            12 =>
+                array (
+                    'id' => 13,
+                    'name' => 'HĐ lao động',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            13 =>
+                array (
+                    'id' => 14,
+                    'name' => 'Thỏa thuận bảo mật thông tin',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            14 =>
+                array (
+                    'id' => 15,
+                    'name' => 'Thỏa thuận thu hồi tạm ứng',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            15 =>
+                array (
+                    'id' => 16,
+                    'name' => 'Đăng ký người phụ thuộc (giấy ủy quyền + Mẫu số:20-ĐK-TCT',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ),
+            16 =>
+                array (
+                    'id' => 17,
                     'name' => '08/CK-TNCN _ Cam kết thuế',
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
