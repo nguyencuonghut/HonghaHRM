633923dc6559c484abc485b8e6e774f3589475ab
Tony Ng <nguyencuonghut55@gmail.com>
2024-08-27 12:31:29 +0700

    Add schedule command to notify when probation contract is expired

---
+++ app/Console/Commands/NotifyProbationContractExpired.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Admin;
+use App\Models\EmployeeContract;
+use App\Notifications\ProbationContractExpired;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Notification;
+
+class NotifyProbationContractExpired extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:notify-probation-contract-expired';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Notify the probation contract expired';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Kiểm tra các hđ thử việc sắp hết hạn trước 7 ngày
+        $employee_contracts = EmployeeContract::where('contract_type_id', 1)
+                                                ->where('status', 'On')
+                                                ->whereDate('end_date', Carbon::now()->addDays(7))
+                                                ->get();
+        // Gửi mail tới nhân viên nhân sự
+        $ns_admins = Admin::where('role_id', 4)->get();
+        foreach ($employee_contracts as $employee_contract) {
+            foreach ($ns_admins as $ns_admin) {
+                Notification::route('mail' , $ns_admin->email)->notify(new ProbationContractExpired($employee_contract->id));
+            }
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\NotifyLaborContractExpired;
+use App\Console\Commands\NotifyProbationContractExpired;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command(NotifyLaborContractExpired::class)->dailyAt('08:10');
+        $schedule->command(NotifyProbationContractExpired::class)->dailyAt('08:20');
     }
 
     /**
